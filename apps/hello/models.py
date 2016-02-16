3b5f32d5b977d7c00e20fb9cd3667b1fccf9ceab
koakumasd <koakumasd@gmail.com>
2016-02-16 12:16:15 +0200

    add bio field

--- apps/hello/models.py
+++ apps/hello/models.py
@@ -4,5 +4,6 @@ class Info(models.Model):
     full_name = models.CharField(max_length=200)
     email = models.CharField(max_length=200)
     phone = models.CharField(max_length=200)
+    bio   = models.TextField()
     def __unicode__(self):
         return self.title
